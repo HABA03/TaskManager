6427d60e4b79344ff6059552dd046f014be303b5
HABA03 <henrybuendia12344@gmail.com>
2026-03-16 14:42:45 -0600

    #3 Se creo el contexto y la implementacion de repositorio de la capa de Infrastructure

---
+++ BACKEND/TM.Infrastructure/Context/TMContext.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using TM.Domain.Entity;
+
+namespace TM.Infrastructure.Context.TMContext;
+
+public class TMContext : DbContext
+{
+    public TMContext()
+    {}
+
+    public TMContext(DbContextOptions<TMContext> options):base(options)
+    {}
+
+    public DbSet<TaskItem> TaskItem {get; set;}
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if(!optionsBuilder.IsConfigured)
+        {
+            var connection = "";
+            optionsBuilder.UseSqlServer(connection);
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TaskItem>(entity =>
+        {
+            entity.HasKey(t => t.Id);
+            entity.Property(t => t.CreatedDate).IsRequired();
+            entity.Property(t => t.Description).IsRequired();
+            entity.Property(t => t.Name).IsRequired();
+            entity.Property(t => t.Status).IsRequired();
+            entity.Property(t => t.UpdatedDate).IsRequired();
+        });
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ BACKEND/TM.Infrastructure/Repositories/TaskItemRepository.cs
@@ -0,0 +1,66 @@
+using Microsoft.EntityFrameworkCore;
+using TM.Domain.Entity;
+using TM.Domain.Enum;
+using TM.Domain.Interface;
+using TM.Infrastructure.Context.TMContext;
+
+namespace TM.Infrastructure.Repositories.TaskItemRepository;
+
+public class TaskItemRepository : ITaskItemRepository
+{
+    private readonly TMContext _context;
+
+    public TaskItemRepository(TMContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<bool> CreateTaskItem(TaskItem taskItem, CancellationToken cancellationToken)
+    {
+        bool response = false;
+        if(taskItem != null)
+        {
+            await _context.TaskItem.AddAsync(taskItem, cancellationToken);
+            response = await _context.SaveChangesAsync(cancellationToken) > 0;   
+        }
+        return response;
+    }
+
+    public async Task<List<TaskItem>> GetAllTaskItem(CancellationToken cancellationToken)
+    {
+        return await _context.TaskItem.Where(t => t.Status != TaskStatusEnum.Deleted).ToListAsync(cancellationToken);
+    }
+
+    public async Task<TaskItem> GetTaskItemById(int id, CancellationToken cancellationToken)
+    {
+        var response = await _context.TaskItem.FirstOrDefaultAsync(t => t.Id == id, cancellationToken);
+        return response != null ? response : new TaskItem();
+    }
+
+    public async Task<bool> RemoveTaskItem(int id, CancellationToken cancellationToken)
+    {
+        bool response = false;
+        if(id > 0)
+        {
+            var currentTask = await _context.TaskItem.FirstOrDefaultAsync(t => t.Id == id, cancellationToken);
+            if(currentTask != null)
+            {
+                _context.TaskItem.Remove(currentTask);
+                response = await _context.SaveChangesAsync(cancellationToken) > 0;
+            }
+        }
+        return response;
+    }
+
+    public async Task<bool> UpdateTaskItem(TaskItem taskItem, CancellationToken cancellationToken)
+    {
+        bool response = false;
+        if(taskItem != null)
+        {
+            taskItem.UpdatedDate = DateTime.UtcNow;
+            _context.TaskItem.Update(taskItem);
+            response = await _context.SaveChangesAsync(cancellationToken) > 0;
+        }
+        return response;
+    }
+}
